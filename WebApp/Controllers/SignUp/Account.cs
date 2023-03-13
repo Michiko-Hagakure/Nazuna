aec5f480e08c9814f03dd0001426b6defec768d6
Michiko Hagakure <shanevitor14@gmail.com>
2023-03-13 09:25:16 +0800

    asp core activity

---
+++ WebApp/Controllers/SignUp/Account.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers.SignUp
+{
+    public class Account : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
